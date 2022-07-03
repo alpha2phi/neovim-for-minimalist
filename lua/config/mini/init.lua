76fcefbad98fed3ff4834df2ab9a993033638999
alpha2phi <alpha2phi@gmail.com>
2022-07-03 20:17:27 +0800

    Update lspconfig

--- lua/config/mini/init.lua
+++ lua/config/mini/init.lua
@@ -1,10 +1,13 @@
+-- default settings
 require("mini.cursorword").setup({})
 require("mini.comment").setup({})
 require("mini.jump").setup({})
 require("mini.jump2d").setup({})
-require("mini.surround").setup()
+require("mini.surround").setup({})
 require("mini.tabline").setup({})
+require("mini.completion").setup({})
 
+-- custom settings
 require("config.mini.base16")
 require("config.mini.starter")
 require("config.mini.statusline")
