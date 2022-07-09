6cd232621c647eb78d9de9899cdbd335d9a3e90a
alpha2phi <alpha2phi@gmail.com>
2022-07-09 10:05:23 +0800

    Add mini featueres

--- lua/config/mini/init.lua
+++ lua/config/mini/init.lua
@@ -6,6 +6,8 @@ require("mini.jump2d").setup({})
 require("mini.surround").setup({})
 require("mini.tabline").setup({})
 require("mini.completion").setup({})
+require("mini.indentscope").setup({})
+require("mini.pairs").setup({})
 
 -- custom settings
 require("config.mini.base16")
