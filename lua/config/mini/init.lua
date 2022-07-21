510b994b870881912045ba888d173a345c498774
alpha2phi <alpha2phi@gmail.com>
2022-07-21 21:20:38 +0800

    feat: Update mini.nvim modules

--- lua/config/mini/init.lua
+++ lua/config/mini/init.lua
@@ -2,12 +2,18 @@
 require("mini.cursorword").setup({})
 require("mini.comment").setup({})
 require("mini.jump").setup({})
-require("mini.jump2d").setup({})
 require("mini.surround").setup({})
 require("mini.tabline").setup({})
 require("mini.completion").setup({})
 require("mini.indentscope").setup({})
 require("mini.pairs").setup({})
+require("mini.jump2d").setup({
+  mappings = {
+    start_jumping = 'S',
+  },
+})
+require('mini.bufremove').setup({})
+require('mini.doc').setup({})
 
 -- custom settings
 require("config.mini.base16")
