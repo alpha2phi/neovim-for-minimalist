6268ae746d9ca2f041f630b31448e8295d3f1abb
alpha2phi <alpha2phi@gmail.com>
2022-08-23 23:59:55 +0800

    feat: package manager and snippets

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -4,19 +4,17 @@ luasnip.config.set_config({ history = true })
 -- Load available snippets
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
 
-print("loaded")
-
 -- Make snippet keymaps
 NVMM.luasnip_go_right = function()
-	if luasnip.expand_or_jumpable() then
-		luasnip.expand_or_jump()
-	end
+  if luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  end
 end
 
 NVMM.luasnip_go_left = function()
-	if luasnip.jumpable() then
-		luasnip.jump(-1)
-	end
+  if luasnip.jumpable() then
+    luasnip.jump(-1)
+  end
 end
 
 vim.api.nvim_set_keymap("i", "<C-l>", [[<Cmd>lua NVMM.luasnip_go_right()<CR>]], {})
@@ -32,17 +30,17 @@ vim.api.nvim_set_keymap("s", "<C-h>", [[<Cmd>lua NVMM.luasnip_go_left()<CR>]], {
 local luasnip_ns = vim.api.nvim_create_namespace("luasnip")
 
 NVMM.luasnip_notify_clear = function()
-	vim.api.nvim_buf_clear_namespace(0, luasnip_ns, 0, -1)
+  vim.api.nvim_buf_clear_namespace(0, luasnip_ns, 0, -1)
 end
 
 NVMM.luasnip_notify = function()
-	if not luasnip.expandable() then
-		NVMM.luasnip_notify_clear()
-		return
-	end
+  if not luasnip.expandable() then
+    NVMM.luasnip_notify_clear()
+    return
+  end
 
-	local line = vim.api.nvim_win_get_cursor(0)[1] - 1
-	vim.api.nvim_buf_set_virtual_text(0, luasnip_ns, line, { { "!", "Special" } }, {})
+  local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+  vim.api.nvim_buf_set_virtual_text(0, luasnip_ns, line, { { "!", "Special" } }, {})
 end
 
 vim.cmd([[au InsertEnter,CursorMovedI,TextChangedI,TextChangedP * lua pcall(NVMM.luasnip_notify)]])
